6c1d94ba020e424a12ecca4ae35edfa977d5f39b
Ahmed Salah <iamahmedsalah@yahoo.com>
2023-05-03 16:16:27 +0300

    Toy-Toon

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.Preloader');
+    loader.classList.add('Preloader-hidden');
+    loader.addEventListener('transitionend',() => {
+        document.body.classList.remove('Preloader')
+    })
+})
---
+++ js/register.js
@@ -0,0 +1,14 @@
+
+  // --------Show Hide Paswword----//
+  const togglePassword = document.querySelector("#togglePassword");
+  const password = document.querySelector("#password");
+
+  togglePassword.addEventListener("click", function () {
+      const type = password.getAttribute("type") === "password" ? "text" : "password";
+      password.setAttribute("type", type);
+      
+      // toggle the icon
+      this.classList.toggle("bi-eye");
+  });
+
+
