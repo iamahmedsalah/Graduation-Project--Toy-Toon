59a76624bb1205583d8c745e4bfb1b33c5bac3fa
Ahmed Salah <94718337+iamahmedsalah@users.noreply.github.com>
2023-05-31 13:55:57 +0300

    Delete ToyToon-v2 directory

--- ToyToon-v2/public/js/cart.js
+++
@@ -1,8 +0,0 @@
-// Preloader
-window.addEventListener('load', () => {
-    const loader = document.querySelector('.Preloader');
-    loader.classList.add('Preloader-hidden');
-    loader.addEventListener('transitionend', () => {
-        document.body.classList.remove('Preloader')
-    })
-})
